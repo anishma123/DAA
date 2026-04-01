ff1d02541a421b842457f6c825268561cf6eb3eb
Anishma Galami <anishmagalami@gmail.com>
2026-04-01 17:00:21 +0545

    adding c++ program

---
+++ GCD.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Function to find GCD
+int gcd(int a, int b) {
+    while (b != 0) {
+        int rem = a % b;
+        a = b;
+        b = rem;
+    }
+    return a;
+}
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "GCD of " << a << " and " << b << " is: " << gcd(a, b);
+
+    return 0;
+}
